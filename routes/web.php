42e2bc837a928314e375edf2ee001a98634e444d
oubbaih <eoubbaih@gmail.com>
2022-06-13 15:12:29 +0100

    hhhsdjdfde

--- routes/web.php
+++ routes/web.php
@@ -30,9 +30,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/download', function () {
   $setting = Setting::all()->first();
-  $file = asset($setting->cv);
+  $file = public_path() . '/' . $setting->cv;
   $header = array(
-    'content-Type:application/pdf'
+    'content-Type' => 'application/pdf'
   );
   if ($file) {
     return Response::download($file, "Download My Resume.PDF", $header);
