6f94fc7597b9f95df302b768e6ca5c671c7cc816
oubbaih <eoubbaih@gmail.com>
2022-06-17 00:11:20 +0100

    asf

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -104,7 +104,7 @@
                                         {{$project->casestudy}}
                                     </h5>
                                     <a class="card-link"
-                                        href="{{route('project.view' , $project)}}">{{__('words.view_case_study')}}<span>&#8594;</span>
+                                        href="{{route('project.first' , $project)}}">{{__('words.view_case_study')}}<span>&#8594;</span>
                                     </a>
                                 </div>
                             </div>
