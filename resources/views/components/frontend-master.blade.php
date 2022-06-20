41c1b12a2e15a8df300582f7fdc641ebf2bcfbe2
oubbaih <eoubbaih@gmail.com>
2022-06-20 12:58:13 +0100

    ds

--- resources/views/components/frontend-master.blade.php
+++ resources/views/components/frontend-master.blade.php
@@ -24,7 +24,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wght@300;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/front-style-rtl-.css')}}">
+    <link rel="stylesheet" href="{{asset('css/front-style-rtl.css')}}">
     @endif
 
     <style>
